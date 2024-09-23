f6807e1354a09d93dde2e944811a75689a46f46d
Norme-Zvenyika <noz224@lehigh.edu>
2024-09-23 19:26:42 -0400

    add the characteristcs id to the main file and pass them as arguments to the PID

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #define GREEN_LED_PIN 16
 #define FLOW_SENSOR_PIN 36
 #define RESET_BUTTON_PIN 39
+#define SERVICE_UUID           "cca082e8-8ed2-4a53-9b7c-ff8f5ffc978d"
+#define CHARACTERISTIC_UUID_RX "ecc6cf21-ed9b-40be-ba42-75ea9ca44b12"
+#define CHARACTERISTIC_UUID_TX "a0817b22-9d2e-40d1-bb9f-ec167871ab76"
 
 // global constants
 const float FILTER_CAPACITY_LITERS = 1.0;              // Filter capacity in liters
@@ -20,7 +23,8 @@ const String DEVICE_NAME = "GE Filter PID";
 
 // Create an instance of the PID class
 PID pidSystem(RED_LED_PIN, YELLOW_LED_PIN, GREEN_LED_PIN, FLOW_SENSOR_PIN, RESET_BUTTON_PIN, 
-              FILTER_CAPACITY_LITERS, PULSES_PER_LITER_CONVERSION, FLOW_RATE_SCALING_FACTOR, DEVICE_NAME);
+              FILTER_CAPACITY_LITERS, PULSES_PER_LITER_CONVERSION, FLOW_RATE_SCALING_FACTOR, 
+              SERVICE_UUID,CHARACTERISTIC_UUID_RX, CHARACTERISTIC_UUID_TX, DEVICE_NAME);
 
 void setup() {
     Serial.begin(115200);
@@ -28,7 +32,8 @@ void setup() {
 }
 
 void loop() {
-    // Update and run the system
     pidSystem.update(); 
     delay(1000);
 }
+
+
